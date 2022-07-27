2ec72ecefba874daeda8ca27c40ca57c569cb7be
tuan <nguyentuan.k58hust@gmail.com>
2022-07-28 00:25:58 +0700

    Change zigzag generation algorithm

--- nav2_behavior_tree/plugins/decorator/feet_detector_node.cpp
+++ nav2_behavior_tree/plugins/decorator/feet_detector_node.cpp
@@ -124,17 +124,15 @@ namespace nav2_behavior_tree
       if (!goals_calculated_)
       {
         tf2::Vector3 leg23_v(leg3_.point.x - leg2_.point.x, leg3_.point.y - leg2_.point.y, 0);
-        double yaw = tf2::tf2Angle(leg23_v, tf2::Vector3(1, 0, 0));
-        // RCLCPP_INFO(node_->get_logger(), "leg23_v %f %f", leg23_v.x(), leg23_v.y());
-        // RCLCPP_INFO(node_->get_logger(), "yaw %f", yaw);
-        tf2::Quaternion leg_23_q(tf2::Vector3(0, 0, 1), yaw);
-        tf2::Transform leg_23(leg_23_q, tf2::Vector3(leg2_.point.x, leg2_.point.y, 0));
+        tf2::Vector3 leg10_v(leg0_.point.x - leg1_.point.x, leg0_.point.y - leg1_.point.y, 0);
+        tf2::Vector3 leg21_v(leg1_.point.x - leg2_.point.x, leg1_.point.y - leg2_.point.y, 0);
+        tf2::Vector3 leg30_v(leg0_.point.x - leg3_.point.x, leg0_.point.y - leg3_.point.y, 0);
+        tf2::Vector3 leg2_v(leg2_.point.x, leg2_.point.y, 0);
+        
         std::vector<tf2::Transform> below_goals_in_legs_frame;
         std::vector<tf2::Transform> beyond_goals_in_legs_frame;
         std::vector<tf2::Transform> cobweb_goals;
         int k = 0;
-        double table_width_ = std::hypot(leg2_.point.x - leg1_.point.x, leg2_.point.y - leg1_.point.y);
-        double table_length = std::hypot(leg23_v.x(), leg23_v.y());
         geometry_msgs::msg::TransformStamped base_wheel_to_base_link = tf_->lookupTransform("base_wheel", "base_link", tf2::TimePointZero, tf2::durationFromSec(1));
         double x1 = base_wheel_to_base_link.transform.translation.x;
         RCLCPP_INFO(node_->get_logger(), "x1 %f", x1);
@@ -144,89 +142,95 @@ namespace nav2_behavior_tree
         RCLCPP_INFO(node_->get_logger(), "x3 %f", x3);
         // Cobweb cleaning
         {
-          double safety = 0.04;
-          tf2::Transform tf = tf2::Transform::getIdentity();
-          tf.setOrigin(tf2::Vector3(x3 + safety, right_side_offset_, 0));
-          tf.setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), 0));
-          cobweb_goals.push_back(tf);
-          tf.setOrigin(tf2::Vector3(table_length - (x3 + safety), right_side_offset_, 0));
-          tf.setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), 0));
-          cobweb_goals.push_back(tf);
-          tf.setOrigin(tf2::Vector3(x3 + safety, right_side_offset_, 0));
-          tf.setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI));
-          cobweb_goals.push_back(tf);
-          tf.setOrigin(tf2::Vector3(x3 + safety, table_width_ - left_side_offset_, 0));
-          tf.setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), 0));
-          cobweb_goals.push_back(tf);
-          tf.setOrigin(tf2::Vector3(table_length - (x3 + safety), table_width_ - left_side_offset_, 0));
-          tf.setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), 0));
-          cobweb_goals.push_back(tf);
-          tf.setOrigin(tf2::Vector3(x3 + safety, table_width_ - left_side_offset_, 0));
-          tf.setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI));
-          cobweb_goals.push_back(tf);
-        }
-        while (true)
-        {
-          double l = right_side_offset_ + robot_width_ * k;
-          if (l > table_width_ - left_side_offset_)
-            break;
-          tf2::Transform tf = tf2::Transform::getIdentity();
-          // if(k == 0) {
-          //   tf.setOrigin(tf2::Vector3(0.15 + 0.06, l, 0));
-          // } else {
-          tf.setOrigin(tf2::Vector3(0.52 + 0.06, l, 0));
-          // }
-          below_goals_in_legs_frame.push_back(tf);
-          tf.setOrigin(tf2::Vector3(table_length - 0.52 - 0.06, l, 0));
-          beyond_goals_in_legs_frame.push_back(tf);
-          k++;
+          double safety = 0.06;
+          // tf2::Vector3 leg_2t = tf2::Transform::getIdentity();
+          tf2::Vector3 delta = leg23_v / leg23_v.length() *  (x3 + safety);
+          // tf2::Vector3 delta_2 = leg21_v / leg21_v.length() * right_side_offset_;
+          // tf_2.setOrigin(leg2_ + delta);
+          // cobweb_goals.push_back(tf_2);
+          // tf2::Transform tf_3 = tf2::Transform::getIdentity();
+          tf2::Vector3 delta_ = leg23_v - delta;
+          // delta_2 = leg30_v / leg30_v.length() * right_side_offset_;
+          // tf_3.setOrigin(leg2_ + delta);
+          // cobweb_goals.push_back(tf_3);
+          // cobweb_goals.push_back(tf_2);
+          // tf2::Transform tf_1 = tf2::Transform::getIdentity();
+          // delta = leg10_v / leg10_v.length() *  (x3 + safety);
+          // delta_2 = - leg21_v / leg21_v.length() * left_side_offset_;
+          // tf_1.setOrigin(leg1_ + delta + delta_2);
+          // cobweb_goals.push_back(tf_1);
+          // tf2::Transform tf_0 = tf2::Transform::getIdentity();
+          // delta = leg10_v - delta;
+          // tf.setOrigin(leg1_ + delta);
+          // cobweb_goals.push_back(tf_0);
+          // tf.setOrigin(tf2::Vector3(x3 + safety, table_width_ - left_side_offset_, 0));
+          // tf.setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI));
+          // cobweb_goals.push_back(tf_1);
+          while (true)
+          {
+            double l = right_side_offset_ + robot_width_ * k;
+            tf2::Vector3 delta_2 = leg21_v / leg21_v.length() * l;
+            tf2::Vector3 delta_3 = leg30_v / leg30_v.length() * l;
+            if (l > leg21_v.length() - left_side_offset_)
+              break;
+            tf2::Transform tf = tf2::Transform::getIdentity();
+            // if(k == 0) {
+            //   tf.setOrigin(tf2::Vector3(0.15 + 0.06, l, 0));
+            // } else {
+            tf.setOrigin( leg2_v + delta + delta_2);
+            // }
+            below_goals_in_legs_frame.push_back(tf);
+            tf.setOrigin(leg2_v + delta_ + delta_3);
+            beyond_goals_in_legs_frame.push_back(tf);
+            k++;
+          }        
         }
 
-        for (unsigned int i = 0; i < below_goals_in_legs_frame.size();)
-        {
-          below_goals_in_legs_frame[i].setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), 0));
-          i++;
-          if (i >= below_goals_in_legs_frame.size())
-            break;
-          below_goals_in_legs_frame[i].setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI / 2));
-          i++;
-        }
-        for (unsigned int i = 0; i < beyond_goals_in_legs_frame.size();)
-        {
-          beyond_goals_in_legs_frame[i].setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI / 2));
-          i++;
-          if (i >= beyond_goals_in_legs_frame.size())
-            break;
-          beyond_goals_in_legs_frame[i].setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI));
-          i++;
-        }
+
+        // for (unsigned int i = 0; i < below_goals_in_legs_frame.size();)
+        // {
+        //   below_goals_in_legs_frame[i].setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), 0));
+        //   i++;
+        //   if (i >= below_goals_in_legs_frame.size())
+        //     break;
+        //   below_goals_in_legs_frame[i].setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI / 2));
+        //   i++;
+        // }
+        // for (unsigned int i = 0; i < beyond_goals_in_legs_frame.size();)
+        // {
+        //   beyond_goals_in_legs_frame[i].setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI / 2));
+        //   i++;
+        //   if (i >= beyond_goals_in_legs_frame.size())
+        //     break;
+        //   beyond_goals_in_legs_frame[i].setRotation(tf2::Quaternion(tf2::Vector3(0, 0, 1), M_PI));
+        //   i++;
+        // }
         // TODO: Calculate list goal from table size and robot size
         nav_msgs::msg::Path path;
-        for (unsigned int i = 0; i < cobweb_goals.size(); i++)
-        {
-          geometry_msgs::msg::PoseStamped goal;
-          goal.header.frame_id = global_frame_;
-          tf2::Transform tf = leg_23 * cobweb_goals[i];
-          tf2::toMsg(tf, goal.pose);
-          goals_.push_back(goal);
-        }
+        // for (unsigned int i = 0; i < cobweb_goals.size(); i++)
+        // {
+        //   geometry_msgs::msg::PoseStamped goal;
+        //   goal.header.frame_id = global_frame_;
+        //   tf2::toMsg(cobweb_goals[i], goal.pose);
+        //   goals_.push_back(goal);
+        // }
         for (unsigned int i = 0; i < below_goals_in_legs_frame.size();)
         {
           geometry_msgs::msg::PoseStamped goal;
           goal.header.frame_id = global_frame_;
-          tf2::Transform tf = leg_23 * below_goals_in_legs_frame[i];
+          tf2::Transform tf = below_goals_in_legs_frame[i];
           tf2::toMsg(tf, goal.pose);
           goals_.push_back(goal);
-          tf = leg_23 * beyond_goals_in_legs_frame[i];
+          tf = beyond_goals_in_legs_frame[i];
           tf2::toMsg(tf, goal.pose);
           goals_.push_back(goal);
 
           if (++i >= below_goals_in_legs_frame.size())
             break;
-          tf = leg_23 * beyond_goals_in_legs_frame[i];
+          tf = beyond_goals_in_legs_frame[i];
           tf2::toMsg(tf, goal.pose);
           goals_.push_back(goal);
-          tf = leg_23 * below_goals_in_legs_frame[i];
+          tf = below_goals_in_legs_frame[i];
           tf2::toMsg(tf, goal.pose);
           goals_.push_back(goal);
           ++i;
@@ -254,6 +258,21 @@ namespace nav2_behavior_tree
         }
         RCLCPP_INFO(node_->get_logger(), "Goals %d finish", i);
       }
+      // for (int i = goals_.size() - 2; i >= 0; i--)
+      // {
+      //   RCLCPP_INFO(node_->get_logger(), "Goals %d", i);
+      //   geometry_msgs::msg::PoseStamped goal = goals_[i];
+      //   goal.header.stamp = node_->get_clock()->now();
+      //   goal.header.frame_id = global_frame_;
+      //   setOutput("goal", goal);
+      //   child_node_->executeTick();
+      //   while (child_node_->waitValidStatus() == BT::NodeStatus::RUNNING)
+      //   {
+      //     child_node_->executeTick();
+      //     // RCLCPP_INFO(node_->get_logger(), "Child node running");
+      //   }
+      //   RCLCPP_INFO(node_->get_logger(), "Goals %d finish", i);
+      // }
       reconfigInflationRadius(last_radius);
       goals_.clear();
       leg0_received = leg1_received = leg2_received = leg3_received = get_all_legs = false;
@@ -277,25 +296,20 @@ namespace nav2_behavior_tree
     param.value.double_value = radius;
     request->parameters.push_back(param);
     auto future_result = set_params_client_->async_send_request(request);
-    callback_group_executor_.spin_some();
-    auto result = future_result.wait_for(10s);
-    if (result == std::future_status::timeout)
+    if (callback_group_executor_.spin_until_future_complete(future_result) !=
+      rclcpp::FutureReturnCode::SUCCESS)
     {
-      RCLCPP_ERROR(node_->get_logger(), "feet_detector_node dynamic reconfigure failed");
-    }
-
-    if (result == std::future_status::ready)
-    {
-      std::vector<rcl_interfaces::msg::SetParametersResult> set_results = future_result.get()->results;
-      for (unsigned int i = 0; i < set_results.size(); i++)
-      {
-        if (!set_results[i].successful)
-        {
-          RCLCPP_ERROR(node_->get_logger(), "Dynamic reconfigure param %s failed reason: %s", request->parameters[i].name.c_str(), set_results[i].reason.c_str());
-        }
-      }
+      throw std::runtime_error("Set local inflation radius failed");
     }
     set_params_client_.reset();
+    set_params_client_ = node_->create_client<rcl_interfaces::srv::SetParameters>
+      ("/global_costmap/global_costmap/set_parameters", rmw_qos_profile_services_default, callback_group_);
+    future_result = set_params_client_->async_send_request(request);
+    if (callback_group_executor_.spin_until_future_complete(future_result) !=
+      rclcpp::FutureReturnCode::SUCCESS)
+    {
+      throw std::runtime_error("Set global inflation radius failed");
+    }
   }
   
   double FeetDetector::getInflationRadius()
@@ -306,18 +320,12 @@ namespace nav2_behavior_tree
     auto request = std::make_shared<rcl_interfaces::srv::GetParameters::Request>();
     request->names.push_back(std::string("inflation_layer.inflation_radius"));
     auto future_result = get_params_client_->async_send_request(request);
-    callback_group_executor_.spin_some();
-    auto result = future_result.wait_for(10s);
-    if (result == std::future_status::timeout)
+    if (callback_group_executor_.spin_until_future_complete(future_result) !=
+        rclcpp::FutureReturnCode::SUCCESS)
     {
-      RCLCPP_ERROR(node_->get_logger(), "feet_detector_node get param failed");
+      throw std::runtime_error("Get parameter failed");
     }
-
-    if (result == std::future_status::ready)
-    {
-      std::vector<rcl_interfaces::msg::ParameterValue> get_results = future_result.get()->values;
-      radius = get_results[0].double_value;
-    }
+    radius = future_result.get()->values[0].double_value;
     return radius;
   }
 
